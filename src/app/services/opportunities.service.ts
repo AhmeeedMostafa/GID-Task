d3fe6380d37fe2d2ab480cd156054584c26f345c
Ahmed Mostafa Khallaf <ahmed.mostafa1198@gmail.com>
2019-02-20 14:08:55 +0200

    Updates for heroku

--- src/app/services/opportunities.service.ts
+++ src/app/services/opportunities.service.ts
@@ -7,7 +7,7 @@ import { Opportunity } from '../classes/opportunity';
   providedIn: 'root'
 })
 export class OpportunitiesService {
-  private mainUrl = 'https://gisapi-web-staging-1636833739.eu-west-1.elb.amazonaws.com/v2/opportunities';
+  private mainUrl = 'http://gisapi-web-staging-1636833739.eu-west-1.elb.amazonaws.com/v2/opportunities';
   private accessToken = 'dd0df21c8af5d929dff19f74506c4a8153d7acd34306b9761fd4a57cfa1d483c';
 
   constructor(private http: HttpClient) {}
